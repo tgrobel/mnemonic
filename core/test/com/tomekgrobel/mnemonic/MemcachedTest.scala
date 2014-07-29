2f4a563c396ccc412040721dd1a475aa02546c54
Tomasz Grobel <tgrobel@gmail.com>
2014-07-29 17:33:40 +0200

    Refactor of integration test

--- core/test/com/tomekgrobel/mnemonic/MemcachedTest.scala
+++ core/test/com/tomekgrobel/mnemonic/MemcachedTest.scala
@@ -19,98 +19,83 @@
 
 package com.tomekgrobel.mnemonic
 
-import org.scalatest.FunSpec
+import org.scalatest.{Tag, FunSpec}
 import play.api.test.{Helpers, FakeApplication}
-import scala.concurrent.Await
+import scala.concurrent.{Awaitable, Await}
 import scala.concurrent.duration._
 import scala.language.postfixOps
 import MemcachedTest._
+import play.api.Application
 
-class MemcachedTest extends FunSpec {
+class MemcachedTest extends InAppFunSpec {
 
-  describe("A Memcached") {
+  describe("A Memcached API object") {
 
     describe("when used in application started without plugin defined") {
 
-      val app: FakeApplication = FakeApplication()
+      implicit val app: FakeApplication = FakeApplication()
 
-      it("should throw exception") {
-        Helpers.running(app) {
-          intercept[MnemonicPluginNotRegistered] {
-            Memcached.add[String](key, value)
-          }
+      in_app_it("should throw exception") {
+        intercept[MnemonicPluginNotRegistered] {
+          Memcached.add[String](key, value)
         }
       }
     }
 
     describe("when used in application started with plugin defined") {
 
-      val app: FakeApplication = FakeApplication(additionalPlugins = Seq("com.tomekgrobel.mnemonic.MnemonicPlugin"))
+      implicit val app: FakeApplication = FakeApplication(additionalPlugins = Seq("com.tomekgrobel.mnemonic.MnemonicPlugin"))
+
+      in_app_it("should be able to add value to Memcached") {
+        val added = Memcached.add[String](key, value).waitForResult
+        assert(added)
+        verifyThatCacheContains(value)
+      }
+
+      in_app_it("should be able to set new value to Memcached") {
+        Memcached.set[String](key, value).waitForResult
+        verifyThatCacheContains(value)
+      }
+
+      in_app_it("should be able to remove value from Memcached") {
+        Memcached.set[String](key, value).waitForResult
+        val deleted = Memcached.delete(key).waitForResult
+        assert(deleted)
+      }
+
+      in_app_it("should be able to change value in Memcached if condition is fulfilled") {
+        Memcached.set[String](key, value).waitForResult
+        val changed = Memcached.compareAndSet[String](key, Some(value), newValue).waitForResult
+        assert(changed)
+      }
+
+      in_app_it("should not be able to change value in Memcached if condition is not fulfilled") {
+        Memcached.set[String](key, value).waitForResult
+        val changed = Memcached.compareAndSet[String](key, None, newValue).waitForResult
+        assert(!changed)
+      }
+
+      in_app_it("should be able to get value and transform it in Memcached") {
+        Memcached.set[String](key, value).waitForResult
+        val result = Memcached.getAndTransform(key) {
+          o: Option[String] => newValue
+        }.waitForResult
+        assert(result === Some(value))
+        verifyThatCacheContains(newValue)
+      }
+
+      in_app_it("should be able to transform value and get it from Memcached") {
+        Memcached.set[String](key, value).waitForResult
+        val result = Memcached.transformAndGet(key) {
+          o: Option[String] => newValue
+        }.waitForResult
+        assert(result === newValue)
+        verifyThatCacheContains(newValue)
+      }
 
       def verifyThatCacheContains(value: String) {
-        val result = Await.result(Memcached.get[String](key), atMost)
+        val result = Memcached.get[String](key).waitForResult
         assert(result === Some(value))
-      }
-
-      it("should be able to add value to Memcache") {
-        Helpers.running(app) {
-          val added = Await.result(Memcached.add[String](key, value), atMost)
-          assert(added)
-          verifyThatCacheContains(value)
-        }
-      }
-
-      it("should be able to set new value to Memcache") {
-        Helpers.running(app) {
-          Await.result(Memcached.set[String](key, value), atMost)
-          verifyThatCacheContains(value)
-        }
-      }
-
-      it("should be able to remove value from Memcache") {
-        Helpers.running(app) {
-          Await.result(Memcached.set[String](key, value), atMost)
-          val deleted = Await.result(Memcached.delete(key), atMost)
-          assert(deleted)
-        }
-      }
-
-      it("should be able to change value in Memcache if condition is fulfilled") {
-        Helpers.running(app) {
-          Await.result(Memcached.set[String](key, value), atMost)
-          val changed = Await.result(Memcached.compareAndSet[String](key, Some(value), newValue), atMost)
-          assert(changed)
-        }
-      }
-
-      it("should not be able to change value in Memcache if condition is not fulfilled") {
-        Helpers.running(app) {
-          Await.result(Memcached.set[String](key, value), atMost)
-          val changed = Await.result(Memcached.compareAndSet[String](key, None, newValue), atMost)
-          assert(!changed)
-        }
-      }
-
-      it("should be able to get value and transform it in Memcache") {
-        Helpers.running(app) {
-          Await.result(Memcached.set[String](key, value), atMost)
-          val result = Await.result(Memcached.getAndTransform(key) {
-            o: Option[String] => newValue
-          }, atMost)
-          assert(result === Some(value))
-          verifyThatCacheContains(newValue)
-        }
-      }
-
-      it("should be able to transform value and get it from Memcache") {
-        Helpers.running(app) {
-          Await.result(Memcached.set[String](key, value), atMost)
-          val result = Await.result(Memcached.transformAndGet(key) {
-            o: Option[String] => newValue
-          }, atMost)
-          assert(result === newValue)
-          verifyThatCacheContains(newValue)
-        }
       }
 
     }
@@ -125,5 +110,24 @@ object MemcachedTest {
   val value: String = "value"
   val newValue: String = "newValue"
 
+  implicit class AwaitSupport[T](awaitable: Awaitable[T]) {
+    def waitForResult: T = {
+      Await.result(awaitable, atMost)
+    }
+  }
+
 }
 
+trait InAppFunSpec extends FunSpec {
+
+  protected val in_app_it = new ItWordInApp
+
+  class ItWordInApp extends ItWord {
+    def apply(specText: String, testTags: Tag*)(testFun: => Unit)(implicit app: Application) {
+      super.apply(specText, testTags: _*) {
+        Helpers.running(app)(testFun)
+      }
+    }
+  }
+
+}
